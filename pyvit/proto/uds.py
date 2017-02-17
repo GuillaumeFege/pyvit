0f41263f57e6d9a81ad255098c6206fcccc8528f
root <guillaume.fege@fesys.fr>
2017-02-17 15:16:21 +0000

    First implements uds

--- pyvit/proto/uds.py
+++ pyvit/proto/uds.py
@@ -1,40 +1,35 @@
-from pyvit.proto.isotp import IsoTpProtocol, IsoTpMessage
-from pyvit.utils.queue import CanQueue
+from pyvit.proto.isotp import *
 import time
 
 
-class UdsInterface(IsoTpProtocol):
-    def __init__(self, can_dev):
-        self.cq = CanQueue(can_dev)
-        self.cq.start()
+class UdsInterface(IsotpNetworkLayer):
+    def __init__(self, dispatcher, padding=0,debug=False):
+        super(UdsInterface,self).__init__(dispatcher,padding,debug)
 
-    def uds_request(self, ecu_id, service, payload, timeout=2):
-        msg = IsoTpMessage(ecu_id)
+    def _request(self,req):
+        ret = self.send_Request(req)
+        if(ret.N_Result != Isotp_N_Result.N_OK):
+            return [False ,"network",ret.N_Result]
+        ret = self.wait_IndicationForConfirm(ret)
+        if(ret.N_Result != Isotp_N_Result.N_OK):
+            return [False ,"network",ret.N_Result]
+        if(ret.data[0] == 127):
+           return [False, "uds", "service not supported"]
+        return [True, ret.data]
 
-        # first byte is service ID, rest of message is payload
-        msg.data = [service] + payload
+    def _createRequest(self,ecuId,payload):
+        return IsotpN_USDataRequest(Isotp_Mtype.diagnostic, ecuId+8, ecuId, Isoyp_N_TAtype.physical, payload)
 
-        # length is payload length plus 1 for service ID byte
-        msg.length = len(payload) + 1
+    def requestSession(self, ecuId, sessionId):
+        payload = [0x10, sessionId]
+        req = self._createRequest(ecuId, payload)
+        return self._request(req)
 
-        # generate a request
-        request = self.generate_frames(msg)
+    def requestInputOutputControlByIdentifier(self, ecuId, identifier, value):
+        idLsb = identifier & 0xFF
+        ifMsb = (identifier >> 8) & 0xFF
+        payload = [0x2F, ifMsb, idLsb, 0x03, value]
+        req = self._createRequest(ecuId, payload)
+        return self._request(req)
+    
 
-        # send the request
-        for f in request:
-            self.cq.send(f)
-
-        # get a response message with the request ID + 40
-        start_ts = time.time()
-        result = None
-
-        while result is None:
-            if time.time() - start_ts > timeout:
-                return None
-
-            response = self.cq.recv(timeout=timeout)
-
-            if response and response.id == ecu_id + 0x40:
-                result = self.parse_frame(response)
-
-        return result
